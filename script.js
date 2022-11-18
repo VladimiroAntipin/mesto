4df9d71e7ff22e4d9196015b49ac5bcc03979825
Vladimiro Antipin <115135530+VladimiroAntipin@users.noreply.github.com>
2022-11-18 13:14:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function toggle() {
+    var overlay = document.getElementById("overlay");
+    overlay.classList.toggle("active");
+    var popup = document.getElementById("popup");
+    popup.classList.toggle("active");
+}
+
+function toggle1() {
+    var likebutton1 = document.getElementById("likebutton1");
+    likebutton1.classList.toggle("active");
+}
+
+function toggle2() {
+    var likebutton2 = document.getElementById("likebutton2");
+    likebutton2.classList.toggle("active");
+}
+
+function toggle3() {
+    var likebutton3 = document.getElementById("likebutton3");
+    likebutton3.classList.toggle("active");
+}
+
+function toggle4() {
+    var likebutton4 = document.getElementById("likebutton4");
+    likebutton4.classList.toggle("active");
+}
+
+function toggle5() {
+    var likebutton5 = document.getElementById("likebutton5");
+    likebutton5.classList.toggle("active");
+}
+
+function toggle6() {
+    var likebutton6 = document.getElementById("likebutton6");
+    likebutton6.classList.toggle("active");
+}
